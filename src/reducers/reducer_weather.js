a0d585dc8467e5fd95c274ab9db542d724addfa7
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-08-25 09:38:25 -0700

    first reducer

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,5 +1,17 @@
-export default function (state = null, action) {
-    console.log('action recieved : ', action)
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function (state = [], action) {
+    console.log('action received : ', action)
+    switch (action.type) {
+        case 'FETCH_WEATHER':
+            // we need to take the current state(list of cities)
+            // and add this new city (action.payload.data) to it
+            // we cannot mutate the state => cannot use 'return state.push(action.payload.data)'
+            // we use concat() to return a new array
+            // can use two approaches in a slightly different way:
+            return state.concat([action.payload.data]);
+            return [action.payload.data, ...state]; // [ city3, city2, city1 ]
+    }
 
     return state;
 };
